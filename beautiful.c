04efdd19947e9528583c289532375c60665fa275
Afolabi <Afolabi>
2023-04-26 03:26:44 -0700

    Fixing Betty errors

---
+++ beautiful.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: This function calculates the sum of two integers
+* and prints the result.
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 5;
+	int y = 4;
+	int sum;
+
+	sum = x + y;
+
+	printf("The sum of %d and %d is %d\n", x, y, sum);
+
+	return (0);
+}
